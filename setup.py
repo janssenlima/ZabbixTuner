5d1f202f2e6ca529bbfe534a490ee733fb6f9ce6
Janssen dos Reis Lima <janssenreislima@gmail.com>
2016-01-28 15:00:54 -0200

    Alteracao no arquivo de setup

--- setup.py
+++ setup.py
@@ -15,19 +15,22 @@ def read(fname):
 setup(
     name='zabbix-tuner',
     url='https://github.com/janssenlima/ZabbixTuner',
-    version='0.1',
+    version='0.1a',
     license='GNU LGPL 2.1',
     author='Janssen dos Reis Lima',
     author_email='janssenreislima@gmail.com',
     description='Zabbix Tuner',
+    py_modules=['ZabbixTuner'],
+    packages=['conf'],
     long_description=read('README.md'),
-    py_modules=['zabbix_tuner'],
     include_package_data=True,
     zip_safe=False,
     platforms='any',
+    keywords='zabbix api tuner tuning',
+    install_requires=['zabbix-api', 'termcolor'],
     classifiers=[
          'Programming Language :: Python',
          'Programming Language :: Python :: 2',
          'Development Status :: 2 - Pre-Alpha',
-        ]
+        ],
 )
